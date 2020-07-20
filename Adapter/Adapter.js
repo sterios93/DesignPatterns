6b17e3ca06e0dd8f765cca73ace10785fcdbf9ab
s.taskudis <s.taskudis@dopamine.bg>
2020-07-20 16:49:37 +0300

    fix: introduce Adapter Design Pattern

---
+++ Adapter/Adapter.js
@@ -0,0 +1,8 @@
+// Getting the ingerface you want from the interface you have
+// A construct which adapts an existing interface X to confrom to the required interface Y
+
+
+//Summary
+// Detemrine the Api you have and the API you need
+// Create a component wich aggreagates (has reference to,) the adaptee
+// Intermediate representations can pile up: use caching and other optimizations
